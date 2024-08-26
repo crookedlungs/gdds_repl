c43dfd03ab7a5db51325651b6973593e5838c407
crookedlungs <ryon.boswell@gmail.com>
2024-08-26 14:14:07 -0500

    f

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted } from "vue";
+import { basicSetup, EditorView } from "codemirror";
+import { keymap } from "@codemirror/view";
+import { defaultKeymap } from "@codemirror/commands";
+import { EditorState } from "@codemirror/state";
+import { python, pythonLanguage } from "@codemirror/lang-python";
+import { language } from "@codemirror/language";
+import { oneDark } from "@codemirror/theme-one-dark";
+
+const editorContainer = ref<HTMLDivElement | null>(null);
+
+onMounted(() => {
+  if (editorContainer.value) {
+    let x = new EditorView({
+      state: EditorState.create({
+        doc: "Hello",
+        extensions: [basicSetup, pythonLanguage, oneDark],
+      }),
+      parent: editorContainer.value,
+    });
+    console.log(x.themeClasses);
+  }
+});
 </script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <p>hi</p>
+  <div ref="editorContainer" class="editor"></div>
 </template>
-
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.editor {
+  height: 300px;
+  width: 300px;
+  border: 1px solid #ddd;
 }
 </style>
