6f1ebb3bb8d488282f135d2c9c505643cfd8deb7
crookedlungs <ryon.boswell@gmail.com>
2024-08-26 15:26:21 -0500

    f

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,14 @@ import { language } from "@codemirror/language";
 import { oneDark } from "@codemirror/theme-one-dark";
 
 const editorContainer = ref<HTMLDivElement | null>(null);
+const editor = ref<EditorView | null>(null);
+const output = ref<string>("");
+
+const pyodide: any = ref();
 
 onMounted(async () => {
   if (editorContainer.value) {
-    let x = new EditorView({
+    editor.value = new EditorView({
       state: EditorState.create({
         doc: "Hello",
         extensions: [
@@ -29,12 +33,31 @@ onMounted(async () => {
 
   // Load Pyodide
   // @ts-ignore
-  await loadPyodide();
+  pyodide.value = await loadPyodide();
 });
+
+const runCode = async () => {
+  try {
+    if (editor.value) {
+      // Get the code from the editor
+      const code = editor.value.state.doc.toString(); // Run the code using Pyodide
+
+      const result = await pyodide.value.runPythonAsync(code);
+      console.log(result.toString);
+      // Set the output
+      output.value = result.toString();
+    }
+  } catch (error) {
+    // @ts-ignore
+    output.value = error.toString();
+  }
+};
 </script>
+
 <template>
   <div style="height: 100%; width: 100%">
     <div ref="editorContainer" class="editor"></div>
+    <button @click="runCode"></button>
   </div>
 </template>
 
